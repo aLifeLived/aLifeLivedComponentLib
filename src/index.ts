98cfb11f1658d3418554eb376a4d750295144638
Max Kelly <maxckel@gmail.com>
2021-07-29 11:43:53 +1000

    Added themes to export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Button from './components/button/Button';
 import { HelpCard } from './components/helpCard/HelpCard';
 import { ThemeProvider } from './themes/ThemeProvider';
+import { Box } from './themes/Box';
+import { Text } from './themes/Text';
 
-export { Button, HelpCard, ThemeProvider };
+export { Button, HelpCard, ThemeProvider, Box, Text };
