fb0c303a833a8b0f97a4f68ac57079fac1c4d5b4
Max Kelly <maxckel@gmail.com>
2021-08-02 22:34:02 +1000

    feat: changes

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   rootDir: './',
   preset: 'react-native',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/__mocks__/globalMock.js'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: ['node_modules/(?!@react-native|react-native)'],
   testPathIgnorePatterns: ['/ios/', '/android/'],
