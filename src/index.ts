eacaa80f0174df29f4549741d3ded81ba61f01f0
Max Kelly <maxckel@gmail.com>
2021-08-03 16:02:08 +1000

    fix: added recording timer to export index

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { HelpCard } from './components/helpCard/HelpCard';
 import { ThemeProvider } from './themes/ThemeProvider';
 import { Box } from './themes/Box';
 import { Text } from './themes/Text';
+import { RecordingTimer } from './components/recordingTimer/RecordingTimer';
 
-export { HelpCard, ThemeProvider, Box, Text };
+export { HelpCard, ThemeProvider, Box, Text, RecordingTimer };
