435a19a7679ccf48b3c85ec75266c8a44f0564ab
Max Kelly <maxckel@gmail.com>
2021-08-03 18:15:58 +1000

    fix: changes

--- src/storybook/storyLoader.js
+++ src/storybook/storyLoader.js
@@ -4,13 +4,13 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-  require('../components/button/Button.stories');
   require('../components/helpCard/HelpCard.stories');
+  require('../components/recordingTimer/RecordingTimer.stories');
 }
 
 const stories = [
-  '../components/button/Button.stories',
   '../components/helpCard/HelpCard.stories',
+  '../components/recordingTimer/RecordingTimer.stories',
 ];
 
 module.exports = {
