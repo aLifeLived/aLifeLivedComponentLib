f9982135c77521378c71ca66454ee8a2d2d48e79
Max Kelly <maxckel@gmail.com>
2021-08-09 20:47:50 +1000

    fix: exporting no cta modal

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,18 @@ import { HelpCard } from './components/helpCard/HelpCard';
 import { ThemeProvider } from './themes/ThemeProvider';
 import { RecordingTimer } from './components/recordingTimer/RecordingTimer';
 import { SignupModal } from './components/signupModal/SignupModal';
+import { NoCtaModal } from './components/noCtaModal/NoCtaModal';
 
 // UI
 import { Box } from './themes/Box';
 import { Text } from './themes/text/Text';
 
-export { HelpCard, ThemeProvider, Box, Text, RecordingTimer, SignupModal };
+export {
+  HelpCard,
+  ThemeProvider,
+  Box,
+  Text,
+  RecordingTimer,
+  SignupModal,
+  NoCtaModal,
+};
