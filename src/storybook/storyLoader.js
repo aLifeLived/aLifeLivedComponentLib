1acaef01534ab1b8827dfbb45ee9cbbf71ff5191
Max Kelly <maxckel@gmail.com>
2021-11-04 21:18:22 +1100

    fix: added missing icons

--- src/storybook/storyLoader.js
+++ src/storybook/storyLoader.js
@@ -13,8 +13,8 @@ function loadStories() {
   require('../components/input/Input.stories');
   require('../components/miniPlayer/MiniPlayer.stories');
   require('../components/noCtaModal/NoCtaModal.stories');
-  require('../components/selectBox/SelectBox.stories');
   require('../components/recordingTimer/RecordingTimer.stories');
+  require('../components/selectBox/SelectBox.stories');
   require('../components/selector/Selector.stories');
   require('../components/signupModal/SignupModal.stories');
   require('../components/storyCard/StoryCard.stories');
@@ -36,8 +36,8 @@ const stories = [
   '../components/input/Input.stories',
   '../components/miniPlayer/MiniPlayer.stories',
   '../components/noCtaModal/NoCtaModal.stories',
-  '../components/optionSelectBox/OptionSelectBox.stories',
   '../components/recordingTimer/RecordingTimer.stories',
+  '../components/selectBox/SelectBox.stories',
   '../components/selector/Selector.stories',
   '../components/signupModal/SignupModal.stories',
   '../components/storyCard/StoryCard.stories',
