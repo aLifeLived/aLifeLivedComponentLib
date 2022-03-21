f8af2fba8d11b66127107be4dae8f8a8f9e6b758
Max Kelly <maxckel@gmail.com>
2022-03-21 16:29:48 +1100

    CU-20xwhxf updated select box to match designs

--- src/components/selectBox/SelectBox.stories.tsx
+++ src/components/selectBox/SelectBox.stories.tsx
@@ -3,6 +3,10 @@ import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
 import { text, boolean } from '@storybook/addon-knobs';
 
+// Assets
+import MyselfRecording from '../../assets/svg/myself-recording.svg';
+import SomeoneElseRecording from '../../assets/svg/someone-else-recording.svg';
+
 // Component
 import { SelectBox } from './SelectBox';
 import { Box } from '../../themes/box/Box';
@@ -11,34 +15,48 @@ import { Icon } from '../icon/Icon';
 storiesOf('Select box', module)
   .addDecorator(Story => Story())
   .add('Horizontal', () => (
-    <Box margin="s">
-      <SelectBox
-        variant="horizontal"
-        title={text('Title', 'Public')}
-        subText={text(
-          'Sub text',
-          'By making your story public other users can listen and share it.'
-        )}
-        isSelected={boolean('isSelected', false)}
-        onPress={action('Public pressed')}
-      >
-        <Icon icon="eye" color="backgroundContrastHigh" size={32} />
-      </SelectBox>
-    </Box>
-  ))
-  .add('Vertical', () => (
-    <Box margin="s">
-      <SelectBox
-        variant="vertical"
-        title={text('Title', 'Public')}
-        subText={text(
-          'Sub text',
-          'By making your story public other users can listen and share it.'
-        )}
-        isSelected={boolean('isSelected', false)}
-        onPress={action('Public pressed')}
-      >
-        <Icon icon="eye" color="backgroundContrastHigh" size={32} />
-      </SelectBox>
+    <Box>
+      <Box margin="m">
+        <SelectBox
+          title={text('Title', 'Public')}
+          subText={text(
+            'Sub text',
+            'By making your story public other users can listen and share it.'
+          )}
+          isSelected={boolean('isSelected', false)}
+          onPress={action('Public pressed')}
+          backgroundColor="secondaryOverlay"
+        >
+          <SomeoneElseRecording />
+        </SelectBox>
+      </Box>
+      <Box margin="m">
+        <SelectBox
+          title={text('Title', 'Public')}
+          subText={text(
+            'Sub text',
+            'By making your story public other users can listen and share it.'
+          )}
+          isSelected={boolean('isSelected', false)}
+          onPress={action('Public pressed')}
+          backgroundColor="primaryOverlay"
+        >
+          <MyselfRecording />
+        </SelectBox>
+      </Box>
+      <Box margin="m">
+        <SelectBox
+          title={text('Title', 'Public')}
+          subText={text(
+            'Sub text',
+            'By making your story public other users can listen and share it.'
+          )}
+          isSelected={boolean('isSelected', false)}
+          onPress={action('Public pressed')}
+          borderColor="primary"
+        >
+          <Icon icon="eye" color="backgroundContrastHigh" size={32} />
+        </SelectBox>
+      </Box>
     </Box>
   ));
