77d953e0ee20d6f4f247bfb123774c66066b48b5
Max Kelly <maxckel@gmail.com>
2022-04-05 22:41:35 +1000

    Added system progress bar to draft card

--- src/components/draftStoryCard/DraftStoryCard.tsx
+++ src/components/draftStoryCard/DraftStoryCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import ProgressBar from 'react-native-animated-progress';
 // UI
 import { Box } from '../../themes/box/Box';
 import { Text } from '../../themes/text/Text';
 import { SystemButton } from '../../themes/systemButton/SystemButton';
 import { Icon } from '../icon/Icon';
+import { SystemProgressBar } from '../../themes/systemProgressBar/SystemProgressBar';
 
 type DraftStoryCardProps = {
   title: string;
@@ -27,43 +28,43 @@ export const DraftStoryCard: React.FC<DraftStoryCardProps> = ({
         flexDirection="row"
         alignContent="center"
         alignItems="center"
-        justifyContent="space-between"
+        justifyContent="space-around"
         boxShadow="sm"
+        paddingBottom="s"
+        overflow={'hidden'}
       >
+        <Box
+          marginLeft="s"
+          marginRight="s"
+          backgroundColor="backgroundContrastLow"
+          padding="s"
+          borderRadius="large"
+        >
+          <Icon icon="pencil-alt" color="primary" />
+        </Box>
         <Box marginLeft="s" marginBottom="s" width="80%">
           <Text
             color="textContrastHigh"
-            variant="body"
+            variant="intro"
             marginTop="s"
             numberOfLines={1}
             marginRight="m"
           >
+            {topicTitle}
+          </Text>
+
+          <Text variant="bodyXSmall" color="textContrastHigh" numberOfLines={1}>
             {title}
           </Text>
-          <Box
-            backgroundColor="primary"
-            alignSelf="flex-start"
-            marginTop="s"
-            padding="xs"
-            borderRadius="medium"
-            paddingLeft="s"
-            paddingRight="s"
-          >
-            <Text
-              variant="bodyXSmall"
-              color="textContrastHigh"
-              numberOfLines={1}
-            >
-              {topicTitle}
-            </Text>
-          </Box>
         </Box>
-        <Icon
-          icon="chevron-right"
-          color="textContrastHigh"
-          size={20}
-          marginRight="m"
-        />
+        <Icon icon="chevron-right" color="primary" size={20} marginRight="m" />
+        <Box width="100%" position="absolute" bottom={0}>
+          <SystemProgressBar
+            progress={100}
+            backgroundColor="primary"
+            height={4}
+          />
+        </Box>
       </Box>
     </SystemButton>
   );
