4f8e0088df90b6374f42989e53e8a92c68446847
Max Kelly <maxckel@gmail.com>
2022-04-11 19:38:35 +1000

    CU-23kn7ub updated topic card to match designs

--- src/components/topicCard/TopicCard.stories.tsx
+++ src/components/topicCard/TopicCard.stories.tsx
@@ -10,19 +10,37 @@ storiesOf('Topic card', module)
   .addDecorator(Story => Story())
   .add('Default topic card', () => (
     <Box>
-      <TopicCard
-        onPress={action('On press fired')}
-        title="Test title"
-        description="This is a test description"
-        duration={90}
-        hasUserCompleted={false}
-      />
-      <TopicCard
-        onPress={action('On press fired')}
-        title="This is a really really long title, here it will break the topic card, if it gets any longer it may break the topic card!!"
-        description="This is a really really really long test description to break the topic card"
-        duration={25}
-        hasUserCompleted={true}
-      />
+      <Box margin="m">
+        <TopicCard
+          onPress={action('On press fired')}
+          title="Ancestry"
+          duration={90}
+          hasUserCompleted={false}
+        />
+      </Box>
+      <Box margin="m">
+        <TopicCard
+          onPress={action('On press fired')}
+          title="This is a really really long title, here it will break the topic card, if it gets any longer it may break the topic card!!"
+          duration={25}
+          hasUserCompleted={true}
+        />
+      </Box>
+      <Box margin="m">
+        <TopicCard
+          onPress={action('On press fired')}
+          title="School Years"
+          duration={25}
+          hasUserCompleted={false}
+        />
+      </Box>
+      <Box margin="m">
+        <TopicCard
+          onPress={action('On press fired')}
+          title="Lessons Learned"
+          duration={25}
+          hasUserCompleted={true}
+        />
+      </Box>
     </Box>
   ));
